f6fc4092ed3f2d44e5ec009bc700c06746e76f04
Arghya <arghyadutta080@gmail.com>
2023-09-15 22:04:40 +0530

    commit merge

--- innovision/src/components/EventCard.js
+++ innovision/src/components/EventCard.js
@@ -5,31 +5,9 @@ import { signInWithGoogleRedirect } from "../config/firebase";
 import Modal from "./Modal";
 
 export const EventCard = ({ title, description, imgUrl }) => {
-<<<<<<< HEAD
-  return (
-    <Col size={12} sm={6} md={4}>
-      <div className="event-imgbx">
-        <img src={imgUrl} />
-        <button className="button">
-          <a href="#forms">
-            <span>Register Now</span>
-          </a>
-        </button>
-        {/* <button className="button2">
-          <span>Guidelines</span>
-        </button> */}
-        <div className="event-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-        </div>
-      </div>
-    </Col>
-  );
-=======
 
     const { currUser, modalState, setModalState } = useContext(UserContext);
-    // const { modalState, setModalState } = useContext(UserContext);
-
+    
     const handleGoogleSignin = async () => {
         try {
             await signInWithGoogleRedirect();
@@ -37,8 +15,6 @@ export const EventCard = ({ title, description, imgUrl }) => {
             console.log(error);
         }
     };
-
-    // const [modalState, setModalState] = useState(false)
 
     const OpenModal = () => {
         console.log(modalState)
@@ -57,7 +33,6 @@ export const EventCard = ({ title, description, imgUrl }) => {
                     ) : (
                         <button className="button" onClick={OpenModal}>
                             <span className=" font-semibold">Register Now</span>
-                            {/* <Modal modalState={modalState} /> */}
                         </button>
 
                     )
@@ -70,5 +45,4 @@ export const EventCard = ({ title, description, imgUrl }) => {
             </div>
         </Col>
     );
->>>>>>> 57d21537b8ae7cdb130e276a7b20f282f3ab535e
 };
