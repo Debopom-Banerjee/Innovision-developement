5be4513ebce8ad3be410f0b4165c2ccf90518215
Debopom-Banerjee <debopombanerjee752@gmail.com>
2023-09-16 15:11:53 +0530

    forms done

--- innovision/src/components/Contact.js
+++ innovision/src/components/Contact.js
@@ -4,7 +4,7 @@ export const Contact = () => {
     <div className="contact" id="contact">
       <Container>
         <Row className="align-items-center">
-          <Col size={15} sm={15}>
+          <Col size={15} sm={20}>
             <h1>For any queries contact us</h1>
             <br />
             <h3>Email :</h3>
@@ -12,15 +12,16 @@ export const Contact = () => {
             <br />
             <h3>Coordinators :</h3>
             <h5 className="coordinators">
-              Swastika Bose:+917044669104 Sansrita Saha:+917003612844 Atalanta
-              Dey:+91 91631 75925 Srijan Mondal:+916294329864 Anamitra Sengupta:
-              +919163708561 Amit RoyChowdury :+917364828740
+              Swastika Bose:+917044669104 &nbsp; Sansrita Saha:+917003612844
+              &nbsp; Atalanta Dey:+919163175925 &nbsp; Srijan
+              Mondal:+916294329864 &nbsp; Anamitra Sengupta: +919163708561
+              &nbsp; Amit RoyChowdury :+917364828740
             </h5>
             <br />
             <h3>Tech Team :</h3>
             <h5 className="techteam">
-              Debopom Banerjee:+919475260487 Arghya Dutta:+919051244326 Shauryam
-              Dubey :+916203940031
+              Debopom Banerjee:+919475260487 &nbsp; Arghya Dutta:+919051244326
+              &nbsp; Shauryam Dubey :+916203940031
             </h5>
           </Col>
         </Row>
