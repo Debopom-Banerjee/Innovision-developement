ce8229ffd94a3a6782a89028837a7761c36bc1ad
lordvader501 <60027612+lordvader501@users.noreply.github.com>
2023-09-19 12:43:07 +0530

    bugs fixed in forms

--- innovision/src/components/Forms.js
+++ innovision/src/components/Forms.js
@@ -9,22 +9,18 @@ import toast from "react-hot-toast";
 
 // import { collection, addDoc } from "firebase/firestore";
 // import { db } from "../../Config/Firebase";
-
+const schema = yup
+  .object()
+  .shape({
+    name: yup.string().required("This is a required field"),
+    year: yup.string().required("This is a required field"),
+    college_roll: yup.string().required("This is a required field"),
+    mobile_no: yup.string().required("This is a required field"),
+    department: yup.string().required("This is a required field"),
+    hack_id: yup.string().default(""),
+  })
+  .required();
 function Forms({ title, setModalState1 }) {
-  const schema = yup
-    .object()
-    .shape({
-      name: yup.string().required("This is a required field"),
-      year: yup.string().required("This is a required field"),
-      college_roll: yup.string().required("This is a required field"),
-      mobile_no: yup.string().required("This is a required field"),
-      department: yup.string().required("This is a required field"),
-      hack_id:
-        title === "Codathon"
-          ? yup.string().required("This is a required field")
-          : yup.string().default(""),
-    })
-    .required();
   const {
     register,
     handleSubmit,
